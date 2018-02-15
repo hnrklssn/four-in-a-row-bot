88543e3d00321df6f9171ef7ae0d7cf3adf2c432
Henrik Olsson <hnrklssn@gmail.com>
2018-02-15 21:50:45 +0100

    Package relevant source code and libs in a zip

---
+++ src/main/scala/util/WeightsConfigContainer.scala
@@ -0,0 +1,8 @@
+package util
+
+/**
+  * Created by henrik on 2017-09-04.
+  */
+object WeightsConfigContainer {
+  val weights: Seq[String] = ??? //GenerateConfigClass.weights("/home/henrik/bots/2017-09-01T05:00")
+}
